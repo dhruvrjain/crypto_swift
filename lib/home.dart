c8965517b9ee3ef9ff81fd756542d8773231c43a
Dhruv R Jain <dhruvrjain01@gmail.com>
2024-01-27 18:50:59 +0530

    Completed Metamask Login and QR

--- lib/home.dart
+++ lib/home.dart
@@ -1,78 +1,92 @@
+import 'package:crypto_swift/appDrawer.dart';
 import 'package:crypto_swift/model/user.dart';
 import 'package:crypto_swift/qr_scanner.dart';
 import 'package:flutter/material.dart';
-import 'package:qr_flutter/qr_flutter.dart';
+import 'package:web3modal_flutter/web3modal_flutter.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key, required this.user});
 
   final User user;
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   void openQrScanner(BuildContext context){
-    Navigator.push(context, MaterialPageRoute(builder: (context)=> QrScanner()));
+    Navigator.push(context, MaterialPageRoute(builder: (context)=> const QrScanner()));
   }
+
+  late W3MService _w3mService;
+
+  @override
+  void initState() {
+    super.initState();
+    initializeState();
+  }
+
+  void initializeState() async {
+    _w3mService = W3MService(
+      projectId: '17e3639e116063d3ba08000ed456d1f8',
+      metadata: const PairingMetadata(
+        name: 'Web3Modal Flutter Example',
+        description: 'Web3Modal Flutter Example',
+        url: 'https://www.walletconnect.com/',
+        icons: ['https://walletconnect.com/walletconnect-logo.png'],
+        redirect: Redirect(
+          native: 'w3m://',
+          universal: 'https://www.walletconnect.com',
+        ),
+      ),
+    );
+    W3MChainPresets.chains.putIfAbsent('80001', () => W3MChainInfo(chainName: 'Polygon Mumbai', chainId: '80001', namespace: 'eip155:80001', tokenName: "MATIC", rpcUrl: "https://rpc-mumbai.maticvigil.com/"));
+    W3MChainPresets.chains.putIfAbsent('80001', () => W3MChainInfo(chainName: 'Test Token 1', chainId: '80001', namespace: 'eip155:80001', tokenName: "TT1", rpcUrl: "https://polygon-mumbai.g.alchemy.com/v2/1Hh1jM_H51CHnUnXDWiYwecKRkGtQhCc"));
+    await _w3mService.init();
+    print(_w3mService.isConnected);
+    print(_w3mService.address);
+    print(_w3mService.chainBalance);
+    print(_w3mService.session);
+    print(_w3mService.avatarUrl);
+
+  }
+  
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (ctx, constraints) => Scaffold(
         appBar: AppBar(title: const Text('CryptoSwift')),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
+        drawer: AppDrawer(user: widget.user),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              DrawerHeader(
-                padding: EdgeInsets.zero,
-                child: UserAccountsDrawerHeader(
-                  margin: EdgeInsets.zero,
-                  accountName: Text(user.name),
-                  accountEmail: Text(user.walletAddress),
-                  currentAccountPicture: Container(
-                    margin: const EdgeInsets.only(bottom: 10),
-                    child: CircleAvatar(
-                      child: Text(
-                        user.name[0],
-                      ),
-                    ),
-                  ),
-                ),
+              const SizedBox(
+                height: 30,
               ),
-              QrImageView(
-                data: '{ walletAddress: ${user.walletAddress} }',
-                // embeddedImage: const AssetImage('assets/images/appLogo.png',),
-                eyeStyle: const QrEyeStyle(color: Color.fromARGB(195, 0, 0, 0),eyeShape: QrEyeShape.square),
-                dataModuleStyle: const QrDataModuleStyle(color: Color.fromARGB(195, 0, 0, 0),dataModuleShape: QrDataModuleShape.circle),
+              ElevatedButton(
+                onPressed: () {
+                  openQrScanner(context);
+                },
+                child: const Text("QRCODE Scanner"),
               ),
-              const ListTile(title: Row(
-                children: [
-                  Icon(Icons.person,size: 26,),
-                  SizedBox(width: 30,),
-                  Text('Profile',style: TextStyle(fontSize: 18,),),
-                ],
-              )),
-              const ListTile(title: Row(
-                children: [
-                  Icon(Icons.settings,size: 26,),
-                  SizedBox(width: 30,),
-                  Text('Settings',style: TextStyle(fontSize: 18,),),
-                ],
-              )),
+              W3MConnectWalletButton(service: _w3mService),
+              const SizedBox(
+                height: 30,
+              ),
+              W3MNetworkSelectButton(service: _w3mService),
+              const SizedBox(
+                height: 30,
+              ),
+              W3MAccountButton(service: _w3mService),
+              const SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(onPressed: (){}, child: Text('Send'),),
+              W3M
             ],
           ),
-        ),
-        body: Column(
-          children: [
-            Text("Sample 1"),
-            SizedBox(
-              height: 30,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                openQrScanner(context);
-              },
-              child: Text("QRCODE Scanner"),
-            )
-          ],
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:crypto_swift/home.dart';
+import 'package:crypto_swift/login.dart';
 import 'package:crypto_swift/model/user.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     const MaterialApp(
-      home: HomeScreen(user: User(id: '1001', name: "Dhruv R Jain", walletAddress: '0xjguwg7tg3288cbw8y8b3b8ybw8db')),
+      home: HomeScreen(
+        user: User(
+          id: '1001',
+          name: "Dhruv R Jain",
+          walletAddress: '0xjguwg7tg3288cbw8y8b3b8ybw8db',
+        ),
+      ),
     ),
   );
 }
+
+// HomeScreen(user: User(id: '1001', name: "Dhruv R Jain", walletAddress: '0xjguwg7tg3288cbw8y8b3b8ybw8db'))
